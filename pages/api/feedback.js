3a05d14371081872f3c67399860d545e48f094a9
James Phelps <james.phelps1995@live.com>
2023-11-07 12:57:32 +0000

    Improve readability for feedback api

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -2,19 +2,27 @@ import prisma from "./prisma/prisma";
 import { allFeedback } from "@/lib/prismaQueries/feedback";
 import * as Validator from "validatorjs";
 
+// CRUD OPERATIONS
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    return createFeedback(req, res);
+  } else if (req.method === "GET") {
+    return getFeedback(req, res);
+  } else if (req.method === "DELETE") {
+    return deleteFeedback(req, res);
+  } else if (req.method === "PUT") {
+    return updateFeedback(req, res);
+  }
+
+  res.status(500).json({ message: "Something went wrong ☹️" });
+}
+
+// Create
 async function createFeedback(req, res) {
   const body = JSON.parse(req.body);
 
   // Validation
-  const validation = new Validator(body, {
-    title: "required|max:150",
-    detail: "required|max:450",
-    category: "required",
-  });
-
-  if (validation.fails()) {
-    res.status(400).json(validation.errors);
-  }
+  validate(body);
 
   // Create data for send via Prisma
   const data = {
@@ -33,114 +41,142 @@ async function createFeedback(req, res) {
   }
 }
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    createFeedback(req, res);
-  } else if (req.method === "GET") {
-    const { feedbackId, category, sort } = req.query;
+// Put
+async function updateFeedback(req, res) {
+  const body = JSON.parse(req.body);
 
-    try {
-      let feedback = null;
+  // Validation
+  validate(body);
 
-      if (feedbackId) {
-        feedback = await prisma.feedback.findUnique({
-          where: {
-            id: parseInt(feedbackId),
-          },
-          select: allFeedback,
-        });
-      } else if (category) {
-        const findManyObj = { select: allFeedback };
+  const data = {
+    title: body.title,
+    detail: body.detail,
+    categoryId: parseInt(body.category),
+    statusId: parseInt(body.status),
+  };
 
-        if (category != "All") {
-          findManyObj.where = {
-            category: {
-              name: category,
-            },
-          };
-        }
-
-        feedback = await prisma.feedback.findMany(findManyObj);
-      } else if (sort) {
-        const findManyObj = { select: allFeedback };
-        const sortBy = sort.toUpperCase();
-
-        if (sortBy == "MOST COMMENTS") {
-          findManyObj.orderBy = {
-            comments: {
-              _count: "desc",
-            },
-          };
-        } else if (sortBy == "LEAST COMMENTS") {
-          findManyObj.orderBy = {
-            comments: {
-              _count: "asc",
-            },
-          };
-        } else if (sortBy == "MOST UPVOTES") {
-          findManyObj.orderBy = {
-            upvotes: {
-              _count: "desc",
-            },
-          };
-        } else {
-          findManyObj.orderBy = {
-            upvotes: {
-              _count: "asc",
-            },
-          };
-        }
-
-        feedback = await prisma.feedback.findMany(findManyObj);
-      }
-
-      res.status(200).json({ feedback });
-    } catch (error) {
-      res.status(400).json({ error });
-    }
-  } else if (req.method === "DELETE") {
-    const { feedbackId } = req.query;
-
-    try {
-      await prisma.feedback.delete({
-        where: {
-          id: parseInt(feedbackId),
-        },
-      });
-    } catch (error) {
-      res.status(400).json({ error });
-    }
-
-    res.status(200).json({ message: "Feedback deleted" });
-  } else if (req.method === "PUT") {
-    const body = JSON.parse(req.body);
-
-    const validation = new Validator(body, {
-      title: "required|max:150",
-      detail: "required|max:450",
-      category: "required",
+  try {
+    await prisma.feedback.update({
+      where: {
+        id: body.feedbackId,
+      },
+      data: data,
     });
 
-    if (validation.fails()) {
-      res.status(400).json(validation.errors);
+    res.status(200).json({ message: "Feedback updated" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
+
+// Get
+async function getFeedback(req, res) {
+  const { feedbackId, category, sort } = req.query;
+
+  try {
+    let feedback = null;
+
+    if (feedbackId) {
+      feedback = await getFeedbackById(feedbackId);
+    } else if (category) {
+      feedback = await getFeedbackByCategory(category);
+    } else if (sort) {
+      feedback = await getFeedbackBySortBy(sort);
     }
 
-    try {
-      await prisma.feedback.update({
-        where: {
-          id: body.feedbackId,
-        },
-        data: {
-          title: body.title,
-          detail: body.detail,
-          categoryId: parseInt(body.category),
-          statusId: parseInt(body.status),
-        },
-      });
+    res.status(200).json({ feedback });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
 
-      res.status(200).json({ message: "Feedback updated" });
-    } catch (error) {
-      res.status(400).json({ error });
-    }
+async function getFeedbackById(feedbackId) {
+  return prisma.feedback.findUnique({
+    where: {
+      id: parseInt(feedbackId),
+    },
+    select: allFeedback,
+  });
+}
+
+async function getFeedbackByCategory(categoryName) {
+  const findManyObj = { select: allFeedback };
+
+  if (categoryName != "All") {
+    findManyObj.where = {
+      category: {
+        name: categoryName,
+      },
+    };
+  }
+
+  return prisma.feedback.findMany(findManyObj);
+}
+
+async function getFeedbackBySortBy(sortText) {
+  const findManyObj = { select: allFeedback };
+  const sortBy = sortText.toUpperCase();
+
+  // Update findManyObj using if else statements
+  orderResultsBySortByText(findManyObj, sortBy);
+
+  return prisma.feedback.findMany(findManyObj);
+}
+
+// Delete
+async function deleteFeedback(req, res) {
+  const { feedbackId } = req.query;
+
+  try {
+    await prisma.feedback.delete({
+      where: {
+        id: parseInt(feedbackId),
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+
+  res.status(200).json({ message: "Feedback deleted" });
+}
+
+// Helper functions
+function validate(data) {
+  const validation = new Validator(data, {
+    title: "required|max:150",
+    detail: "required|max:450",
+    category: "required",
+  });
+
+  if (validation.fails()) {
+    res.status(400).json(validation.errors);
+  }
+}
+
+function orderResultsBySortByText(results, sortText) {
+  if (sortText == "MOST COMMENTS") {
+    results.orderBy = {
+      comments: {
+        _count: "desc",
+      },
+    };
+  } else if (sortText == "LEAST COMMENTS") {
+    results.orderBy = {
+      comments: {
+        _count: "asc",
+      },
+    };
+  } else if (sortText == "MOST UPVOTES") {
+    results.orderBy = {
+      upvotes: {
+        _count: "desc",
+      },
+    };
+  } else {
+    results.orderBy = {
+      upvotes: {
+        _count: "asc",
+      },
+    };
   }
 }
